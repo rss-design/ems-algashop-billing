408c5c98fe3abf5fbec332902882e087659d603b
rafa.santos <rafael@magazineluiza.com.br>
2026-03-22 12:44:14 -0300

    5.12: Enhancing Customer API responses

--- src/main/java/com/algaworks/algashop/billing/BillingApplication.java
+++ src/main/java/com/algaworks/algashop/billing/BillingApplication.java
@@ -1,5 +1,7 @@
 package com.algaworks.algashop.billing;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BillingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BillingApplication.class, args);
+      TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+      SpringApplication.run(BillingApplication.class, args);
 	}
 
 }
